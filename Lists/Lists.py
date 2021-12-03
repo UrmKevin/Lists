cf886e050e3111e4b7a91d30fe6770b200b4876f
UrmKevin <93324340+UrmKevin@users.noreply.github.com>
2021-12-03 10:02:42 +0200

    Update Lists.py

--- Lists/Lists.py
+++ Lists/Lists.py
@@ -82,19 +82,12 @@
 #for i in range(N):
 #    l.append(randint(1,20))
 #print(l)
-#max=1
-#for element in l:
-#    if element>max: 
-#        max=element
-#        y=l.index(max)
-#x=max/N
-#l.pop(y)
-#l.insert(y,x)
 #nl=[]
 #for e in l:
 #    nl.append(abs(e))
 #print(nl.sort())
 #print(nl.sort(reverse=True))
+#print(nl)
 
 # number 5
 #l = ['крот', 'белка', 'выхухоль']
@@ -104,9 +97,83 @@
 #	x=len(l[i])
 #	if x>max:
 #		max=x
-
 #for i in range(len(l)):
 #	tl=l[i]
 #	nl.append(tl.ljust(max,'_'))
-
 #print(nl)
+
+#l = ['крот', 'белка', 'выхухоль']
+#nl=[]
+#max=0
+#for e in l:
+#	x=len(e)
+#	if x>max:
+#		max=x
+#for e in l:
+#	nl.append(e.ljust(max,'_'))
+#print(nl)
+
+#l1=['крот', 'белка', 'выхухоль']
+#l2=['a', 'aa', 'aaa', 'aaaa', 'aaaaa']
+#l3=['qweasdqweas', 'q', 'rteww', 'ewqqqqq']
+#sss=[l1,l2,l3]
+#c=0
+#for u in sss:
+#	print(sss[c])
+#	max=0
+#	nl=[]
+#	for e in sss[c]:
+#		x=len(e)
+#		if x>max: max=x
+#	for e in sss[]:
+#		nl.append(e.ljust(max,'_'))
+#	print(nl)
+#	c+=1
+
+# number 6
+name="0s"
+gl=["e","u","i","o","a"]
+cnt1=0
+cnt2=0
+while name.isalpha()==False:
+    try:
+        name=str(input("Name(eng): "))
+    except: ValueError
+l=list(name)
+name=name.lower()
+print("Hello, ",name.capitalize(),"!!")
+print("You have ",len(l),"letters in name")
+
+dub=[x for x in l if l.count(x)>=1]
+dub=list(set(dub))
+
+print(sorted(dub))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# rock papper scissors
+#from random import*
+#l=["rock","papper","scissors"]
+#name=input("Write your name: ")
+#print("1 - ",l[0],", 2 - ",l[1],", 3 - ",l[2])
+#p=input("")
+#if p == 1 or p == 2 or p == 3:
+
